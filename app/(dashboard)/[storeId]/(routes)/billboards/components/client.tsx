1047ba2dbf82db740e9a5bbd768f58009c261c92
unknown <anna.ruzimova@iis.utm.md>
2024-09-23 16:45:42 +0300

    creating data table

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -5,15 +5,21 @@ import { useParams, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
 
-export const BillboardClient = () => {
+import { BillboardColumn, columns } from "./colums";
+
+interface BillboardCleintProps {
+  data: BillboardColumn[];
+}
+export const BillboardClient: React.FC<BillboardCleintProps> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
   return (
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title="Billboards (0)"
+          title={`Billboards (${data.length})`}
           description="Manage billboards for your store"
         />
         <Button
@@ -24,6 +30,7 @@ export const BillboardClient = () => {
         </Button>
       </div>
       <Separator />
+      <DataTable columns={columns} data={data} />
     </>
   );
 };
