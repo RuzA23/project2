ba6efb7bde1ebfee11ca8a7fe0e7ae269f18f75c
unknown <anna.ruzimova@iis.utm.md>
2024-09-29 12:22:06 +0300

    end of adding to cart

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 
 import "./globals.css";
+import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
 
 const font = Urbanist({
   subsets: ["latin"],
@@ -22,6 +24,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider />
+        <ToastProvider />
         <Navbar />
         {children}
         <Footer />
